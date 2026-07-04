624eb2427f6ad190468c0076c08c792cd0ca0b7f
Veka0 <26331365+Veka0@users.noreply.github.com>
2026-07-04 02:01:14 +0200

    1.21.110.25

--- materials/StandardRed/Opaque.Vertex.glsl
+++ materials/StandardRed/Opaque.Vertex.glsl
@@ -16,7 +16,6 @@
 *
 * Buffers:
 * - uniform lowp sampler2D s_MatTexture;
-* - uniform lowp sampler2D s_ShadowTexture;
 *
 * Uniforms:
 * - uniform vec4 Ambient;
@@ -24,9 +23,6 @@
 * - uniform vec4 LightDiffuseColorAndIlluminance;
 * - uniform vec4 LightWorldSpaceDirection;
 * - uniform vec4 MaterialID;
-* - uniform vec4 ShadowFilterSize;
-* - uniform vec4 ShadowTexel;
-* - uniform mat4 ShadowTransform;
 */
 
 uniform mat4 u_modelView;
@@ -51,7 +47,7 @@ out vec4 v_viewSpacePosition;
 out vec3 v_worldPos;
 void main() {
 #ifdef INSTANCING__OFF
-    vec4 var_dc0f7 = u_model[0] * vec4(a_position, 1.0);
+    vec4 var_400c1 = u_model[0] * vec4(a_position, 1.0);
 #endif
 #ifdef INSTANCING__ON
     vec4 var_78b44 = i_data1;
@@ -62,14 +58,12 @@ void main() {
     var_e43a8[1] = vec4(var_78b44.y, var_e67a8.y, var_1b7f0.y, 0.0);
     var_e43a8[2] = vec4(var_78b44.z, var_e67a8.z, var_1b7f0.z, 0.0);
     var_e43a8[3] = vec4(var_78b44.w, var_e67a8.w, var_1b7f0.w, 1.0);
-    vec4 var_dc0f7 = var_e43a8 * vec4(a_position, 1.0);
+    vec4 var_400c1 = var_e43a8 * vec4(a_position, 1.0);
 #endif
-    vec3 var_d0d3b = var_dc0f7.xyz;
-    vec3 var_1b63d = var_d0d3b;
     v_color0 = a_color0;
     v_texcoord0 = a_texcoord0;
     v_viewSpaceNormal = (u_modelView * vec4(a_normal.xyz, 0.0)).xyz;
-    v_viewSpacePosition = u_view * vec4(var_1b63d.x, var_1b63d.y, var_1b63d.z, 1.0);
-    v_worldPos = var_d0d3b;
-    gl_Position = u_viewProj * vec4(var_dc0f7.xyz, 1.0);
+    v_viewSpacePosition = u_view * vec4(var_400c1.xyz, 1.0);
+    v_worldPos = var_400c1.xyz;
+    gl_Position = u_viewProj * vec4(var_400c1.xyz, 1.0);
 }
